ff9e88ba222274a1fa812e7e1afcdcd97eaaf3a4
Iain <iainmilliken@gmail.com>
2021-08-23 18:02:31 -0700

    help

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,48 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var passwordcharacters = '';
+  var passwordLength = prompt('Please Enter a number bwtween 8 and 128')
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please select a number between 1 and 128")
+    return "this would be the password"
+  }
+  console.log(passwordLength)
+  var passwordLowercase = confirm('Allow Lower case')
+  if (passwordLowercase) {
+    passwordcharacters += 'abcdefghijklmnopqrstuvwxyz'
+  }
+  console.log("Lowercase:",passwordLowercase)
+  var passwordUppercase = confirm('Allow Upper case')
+  if (passwordUppercase) {
+    passwordcharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+  console.log('Uppercase:',passwordUppercase)
+  var passwordNumeric = confirm('Allow Numbers')
+  if (passwordNumeric)  {
+    passwordcharacters += '1234567890'
+  }
+  console.log('Numeric:',passwordNumeric)
+  var passwordSpecial = confirm('Allow Special Characters')
+  if (passwordSpecial)  {
+    passwordcharacters += '!@#$%^&*<>?-=_+'
+  }
+  console.log('Special Characters:',passwordSpecial)
+  console.log(passwordcharacters)
+
+
+  return passwordVariable
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  for (let i = 0; passwordcharacters; i++) {
+    writePassword += password[i] + "<br>";
+  }
   passwordText.value = password;
-
+ 
 }
 
 // Add event listener to generate button
